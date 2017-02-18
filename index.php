ba5d25ae0817c40e9204a9cd07e9713491cff591
Guillaume Boufflers <guillaume.boufflers@critizr.com>
2017-02-18 12:28:05 +0100

    redirect fix

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-//include_once("home.html");
-
 if ($_GET['page'] == 'flunch'){
     header('Location: http://www.flunch.com/');
 } else if ($_GET['page'] == 'carrefour'){
     header('Location: http://www.carrefour.com/');
 } else{
-    header('Location: http://www.critizr.com/');
+    include_once("home.html");
 }
 
 ?>
